80e8ac160347578825b7e3dde6f1af30c1475da0
Filiuk Artem <artemfiluk41@gmail.com>
2025-11-16 23:57:52 +0200

    FEAT: Add hello function

---
+++ hello.h
@@ -0,0 +1,13 @@
+// contains my hello function declaration
+#pragma once
+
+// include guards
+#ifndef HELLO_H_
+#define HELLO_H_
+
+namespace hello {
+	// prints message on the screen
+	void print_hello();
+}
+
+#endif // HELLO_H_
